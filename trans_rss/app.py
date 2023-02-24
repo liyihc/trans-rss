9e56f897d9be38a3a476013f4f27377c0bea18be
liyihc <liyihc@outlook.com>
2023-02-24 10:44:58 +0800

    add early stop

--- trans_rss/app.py
+++ trans_rss/app.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import FastAPI
 import httpx
 from fastapi_utils.tasks import repeat_every
+from pydantic import BaseModel
 from .config import version, config
 from .sql import Subscribe, Connection
 from . import webhooks
@@ -33,10 +34,17 @@ async def hello(sql_statement: str):
         return cursor.fetchall()
 
 
-@app.post("/api/subscribe")
+class Torrent(BaseModel):
+    title: str
+    url: str
+
+
+@app.post("/api/subscribe", response_model=List[Torrent])
 async def subscribe(name: str, url: str):
     with Connection() as conn:
         conn.subscribe(name, url)
+        sub = Subscribe(name=name, url=url)
+        return [Torrent(title=title, url=torrent) async for title, torrent in subscribe_url(sub)]
 
 
 @app.delete("/api/subscribe")
@@ -49,6 +57,12 @@ async def subscribe(name: str):
 async def subscribe():
     with Connection() as conn:
         return conn.subscribe_get()
+
+
+@app.post("/api/has_download")
+async def has_download(torrent: str):
+    with Connection() as conn:
+        return conn.download_add(torrent)
 
 
 @app.post("/api/start")
@@ -67,60 +81,62 @@ title_pattern = re.compile(r'<title>([^<>]*)</title>')
 torrent_pattern = re.compile(r'http[^"]*\.torrent')
 
 
+async def subscribe_url(sub: Subscribe):
+    async with httpx.AsyncClient() as client:
+        page = 1
+        retry = 0
+        while True:
+            req = await client.get(f"{sub.url}&page={page}")
+            match req.status_code:
+                case 500:  # page end
+                    return
+                case 200:
+                    retry = 0
+                    cnt = 0
+                    it = title_pattern.finditer(req.text)
+                    next(it)
+                    for title, torrent in zip(
+                            it,
+                            torrent_pattern.finditer(req.text)):
+                        title = title.group(1)
+                        torrent = torrent.group()
+                        cnt += 1
+                        yield title, torrent
+                    if not cnt:
+                        return
+                    page += 1
+                case _:
+                    retry += 1
+                    if retry == 10:
+                        return
+
+
+async def broadcast(name: str, title: str, torrent: str):
+    async with httpx.AsyncClient() as client:
+        for webhook in config.webhooks:
+            resp = await client.post(webhook, json=webhooks.feishu(name, title, torrent))
+            print("webhook", webhook, resp.status_code)
+            # TODO log when failed
+
+
 @app.post("/api/manual_update")
 async def update():
     ret = []
     trans_client = config.trans_client()
     with Connection() as conn:
-        async with httpx.AsyncClient() as client:
-            for subscribe in conn.subscribe_get():
-                print("subscribe", subscribe.name, subscribe.url)
-                page = 1
-                retry = 0
-                while True:
-                    req = await client.get(f"{subscribe.url}&page={page}")
-                    print(subscribe.name, f"page:{page}")
-                    match req.status_code:
-                        case 500:  # page end
-                            retry = 0
-                            break
-                        case 200:
-                            retry = 0
-                            cnt = 0
-                            it = title_pattern.finditer(req.text)
-                            next(it)
-                            for title, torrent in zip(
-                                    it,
-                                    torrent_pattern.finditer(req.text)):
-                                title = title.group(1)
-                                torrent = torrent.group()
-                                cnt += 1
-                                print("find", subscribe.name, title, torrent)
-                                if not conn.download_exist(torrent):
-                                    ret.append(torrent)
-                                    print("download", subscribe.name,
-                                          title, torrent)
-                                    for webhook in config.webhooks:
-                                        resp = await client.post(webhook, json=webhooks.feishu(
-                                            subscribe.name, title, torrent
-                                        ))
-                                        print(webhook, resp.status_code)
-                                        # TODO log when failed
+        for sub in conn.subscribe_get():
+            print("subscribe", sub.name, sub.url)
+            async for title, torrent in subscribe_url(sub):
+                if conn.download_exist(torrent):
+                    print("torrent exist:", sub.name, title, torrent)
+                    print("subscribe stop", sub.name)
+                    break
+                print("download", sub.name, title, torrent)
+                await broadcast(sub.name, title, torrent)
 
-                                    # TODO log when add
-                                    t = trans_client.add_torrent(torrent, download_dir=str(
-                                        config.base_folder / subscribe.name))
-                                    conn.download_add(torrent)
-                                else:
-                                    print("skip", subscribe.name, title, torrent)
-
-                            if not cnt:
-                                break
-                            page += 1
-                        case _:
-                            retry += 1
-                            if retry == 10:
-                                break
+                t = trans_client.add_torrent(torrent, download_dir=str(
+                    config.base_folder / sub.name))
+                conn.download_add(torrent)
     return ret
 
 
